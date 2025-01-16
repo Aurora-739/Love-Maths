f64c8fe31e265ba72b70e155dfb3b8122368f7b8
Aurora Keay <yfifej9y@students.codeinstitute.net>
2025-01-16 11:36:10 +0000

    add the code to create 2 random numbers for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function(){ //wait for DOM content to load before starign the js
+document.addEventListener("DOMContentLoaded", function () { //wait for DOM content to load before starign the js
     let buttons = document.getElementsByTagName("button"); // 5 buttons (4 game btns and 1 sumbit)
 
-    for (let button of buttons){ //returns and stores each element in the array to the variable "button" on each iteration.
-        button.addEventListener("click", function(){ //event listeer waits for the singular button to be clicked.
+    for (let button of buttons) { //returns and stores each element in the array to the variable "button" on each iteration.
+        button.addEventListener("click", function () { //event listeer waits for the singular button to be clicked.
             if (this.getAttribute("data-type ") === "submit") { // checks the contents of the attribute
                 alert("You clicked sumbit");
             } else {
@@ -11,13 +11,19 @@ document.addEventListener("DOMContentLoaded", function(){ //wait for DOM content
             }
         })
     }
-}) 
-
+})
+/**
+ * The main game "loop", called when the script is first loaded,
+ * and after the user's answer has been processed.  
+ */
 function runGame() {
 
+    //creates 2 random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
-function checkAnswer () {
+function checkAnswer() {
 
 }
 
@@ -40,9 +46,9 @@ function displayAdditionQuestion() {
 }
 
 function displaySubtractQuestion() {
-    
+
 }
 
 function displayMultiplyQuestion() {
-    
+
 }
