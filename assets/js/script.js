cea520aed9a6bde9c7de36fddf1e17eafa4a1b83
Aurora Keay <yfifej9y@students.codeinstitute.net>
2025-01-16 11:30:07 +0000

    start to add event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(){ //wait for DOM content to load before starign the js
+    let buttons = document.getElementsByTagName("button"); // 5 buttons (4 game btns and 1 sumbit)
+
+    for (let button of buttons){ //returns and stores each element in the array to the variable "button" on each iteration.
+        button.addEventListener("click", function(){ //event listeer waits for the singular button to be clicked.
+            if (this.getAttribute("data-type ") === "submit") { // checks the contents of the attribute
+                alert("You clicked sumbit");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+}) 
+
 function runGame() {
 
 }
@@ -17,6 +32,8 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
+
+// QUESTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 function displayAdditionQuestion() {
 
