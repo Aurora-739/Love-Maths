fc388743cad52f1d7fa6322211a483064dc9dbc7
Aurora Keay <yfifej9y@students.codeinstitute.net>
2025-01-17 15:34:00 +0000

    tidy up code

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,11 @@ document.addEventListener("DOMContentLoaded", function () { //wait for DOM conte
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
     runGame("addition");
 
 });
@@ -20,6 +25,10 @@ document.addEventListener("DOMContentLoaded", function () { //wait for DOM conte
  * and after the user's answer has been processed.  
  */
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     //creates 2 random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
